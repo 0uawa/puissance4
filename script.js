9d1adba674aa8adeffa52c15551880987f1484c8
Eliott Letouze <eliott0503@gmail.com>
2021-02-01 18:37:43 +0100

    creationFichier

---
+++ script.js
@@ -0,0 +1,7 @@
+class puissance4{
+        constructor(ligne,colonne,premierJoueur,couleur,){
+            void render() ;
+        }
+        let jeu = document.createElement("table");
+                 
+}
